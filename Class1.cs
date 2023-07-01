efbaf3e65246047d43f7d8a89cd49b05586dbb5e
Oleg Chumin <tschumin.oleg@gmail.com>
2023-07-01 09:50:36 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity_C__programming_for_Beginners
+{
+    internal class Class1
+    {
+    }
+}
